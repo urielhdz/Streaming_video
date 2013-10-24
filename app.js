ea11af2b9db5679d564625ecff5c076f5dbe1bba
galera <adrian.galera.87@gmail.com>
2013-10-24 17:18:46 +0200

    First commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var server = http.createServer(app);
 server.listen(8080);
 var io = require("socket.io").listen(server);
+io.set('log level','1')
 io.sockets.on('connection',function(socket){
 	socket.on('newFrame',function(img){
 		io.sockets.emit('setFrame',img);
